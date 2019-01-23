b52e9f4835ebf7584ab74f30f5f3714f2405b132
Christopher Kraus <chkrau@pool07.pool.tp1.hhu.de>
2019-01-23 12:55:13 +0100

    Thomas rockt hart!

--- BorisPushRel.cxx
+++ BorisPushRel.cxx
@@ -8,19 +8,59 @@ class particle{
 public:
 	particle(){x=0; y=0; z=0;  // location at t = 0
 		         px=0; py=0; pz=0;  // momentum at t = -dt
-						 gam=0;  // gamma at t= -dt};
+						 gam=0;};  // gamma at t= -dt};
 	// Print particle position, momentum u and gamma-factor
 	void print_info(const double t) const;
 	
-  double get_x() const{return x;}
+    double get_x() const{return x;}
+    void boris_push(double Ex, double Ey, double Ez, double Bx, double By, double Bz, double dt);
 private:
 	double x,y,z;
 	double px,py,pz;
-  double gam;
+    double gam;
 	// Calculate gamma-factor from current momentum
-
+    void calcgam();
 };
 //----------------------------------
+void particle::calcgam(){
+    gam = sqrt(1+(px*px+py*py+pz*pz));
+}
+
+void particle::boris_push(double Ex, double Ey, double Ez, double Bx, double By, double Bz, double dt){
+    //calcgam();
+    
+    double pminx = px+dt/2*Ex;
+    double pminy = py+dt/2*Ey;
+    double pminz = pz+dt/2*Ez;
+    
+    calcgam();
+    
+    double Tx = (Bx/gam)*(dt/2);
+    double Ty = (By/gam)*(dt/2);
+    double Tz = (Bz/gam)*(dt/2);
+    
+    double pstrichx = pminx + (pminy*Tz - pminz*Ty);
+    double pstrichy = pminy + (pminz*Tx - pminx*Tz);
+    double pstrichz = pminz + (pminx*Ty - pminy*Tx);
+    
+    double sx = 2*Tx/(1+(Tx*Tx+Ty*Ty+Tz*Tz));
+    double sy = 2*Ty/(1+(Tx*Tx+Ty*Ty+Tz*Tz));
+    double sz = 2*Tz/(1+(Tx*Tx+Ty*Ty+Tz*Tz));
+    
+    double pplusx = pminx + (pstrichy*sz - pstrichz*sy);
+    double pplusy = pminy + (pstrichz*sx - pstrichx*sz);
+    double pplusz = pminz + (pstrichx*sy - pstrichy*sx);
+    
+    px = pplusx + dt/2*Ex;
+    py = pplusy + dt/2*Ey;
+    pz = pplusz + dt/2*Ez;
+    
+    calcgam();
+    
+    x += dt* px/gam;
+    y += dt* py/gam;
+    z += dt* pz/gam;
+}
 //----------------------------------
 int main(){
   const double FWHM = 10;
